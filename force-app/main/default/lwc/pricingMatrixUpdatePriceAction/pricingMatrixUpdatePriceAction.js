3803fb842f4c19f2d6cda44c287eb7604a13d3df
Micah Gerger <micah@bluerobotconsulting.com>
2026-06-08 11:36:16 -0400

    Update price id fix

--- force-app/main/default/lwc/pricingMatrixUpdatePriceAction/pricingMatrixUpdatePriceAction.js
+++ force-app/main/default/lwc/pricingMatrixUpdatePriceAction/pricingMatrixUpdatePriceAction.js
@@ -1,18 +1,34 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import { CloseActionScreenEvent } from "lightning/actions";
 import { notifyRecordUpdateAvailable } from "lightning/uiRecordApi";
 
 export default class PricingMatrixUpdatePriceAction extends LightningElement {
-  @api recordId;
+  @track _showFlow = false;
+  _recordId;
+
+  @api
+  get recordId() {
+    return this._recordId;
+  }
+  set recordId(value) {
+    this._recordId = value;
+    if (value) {
+      this._showFlow = true;
+    }
+  }
+
+  get showFlow() {
+    return this._showFlow;
+  }
 
   get flowInputVariables() {
-    return [{ name: "recordId", type: "String", value: this.recordId }];
+    return [{ name: "recordId", type: "String", value: this._recordId }];
   }
 
   async handleStatusChange(event) {
     const status = event.detail.status;
     if (status === "FINISHED" || status === "FINISHED_SCREEN") {
-      await notifyRecordUpdateAvailable([{ recordId: this.recordId }]);
+      await notifyRecordUpdateAvailable([{ recordId: this._recordId }]);
       this.dispatchEvent(new CloseActionScreenEvent());
     }
   }
